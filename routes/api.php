46469617a4e21fde1e49b8e75cafdb99b50e35aa
Raul302 <luis_1547@hotmail.com>
2019-09-13 17:57:57 -0500

    token generado

---
+++ routes/api.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+// // Route::middleware('auth:api')->get('/user', function (Request $request) {
+// // });
+
+Route::middleware('auth:api')->get('/logear', function (Request $request) {
+    return $request->user();
+
+});
+
+Route::post('/generartoken',function(){
+    $token = Str::random(60);
+
+    $User = App\User::find(1);
+
+    $User['api_token'] =hash('sha256', $token);
+
+    $User->save();
+    
+    return $token;
+});
+// lRerWCdRa6ZpRkTyoPOCHy7eupqjF8TAQ9oS5avRx2gmQxbRI6hpTY0vYwz0
+
+// Use illuminate SUPOORT STR;
+// $User = App slash User::find(1);
+// return $user;
+// Migracion,Seeder,Brincar middleware,Guardar en BD
